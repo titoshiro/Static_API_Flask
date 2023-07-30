a4fdb68b1c1235784d8a5cc16927c6552894c199
Your Name <you@example.com>
2023-07-29 20:27:07 -0400

    13 de 13

--- src/app.py
+++ src/app.py
@@ -1,70 +1,69 @@
+
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
-def get_all_members():
+def handle_hello():
+
+   
     members = jackson_family.get_all_members()
-    if members is not None:
-        return jsonify(members), 200
-    else:
-        return jsonify({"error": "Not found"}), 404
-    
+    members = list(map(lambda member: member, members))
+
+    return jsonify(members), 200
+
 
 @app.route('/member/<int:member_id>', methods=['GET'])
 def get_member(member_id):
     member = jackson_family.get_member(member_id)
-    if member is not None:
+    if member:
         return jsonify(member), 200
     else:
-        return jsonify({"message": "Member not found"}), 404
+        return jsonify({"error": "Member not found"}), 404
+
+
 
 @app.route('/member', methods=['POST'])
 def add_member():
     data = request.get_json()
-
-    if data is None or "first_name" not in data or "age" not in data or "lucky_numbers" not in data:
-        return jsonify({"error": "Bad request"}), 400
-
-    if "id" not in data:
-        data["id"] = None
-
     jackson_family.add_member(data)
-    return jsonify(), 200
+    return jsonify({}), 200
 
-@app.route('/member/<int:member_id>', methods=['PUT'])
-def update_member(member_id):
-    data = request.get_json()
-    if jackson_family.update_member(member_id, data):
-        return jsonify(), 200
-    else:
-        return jsonify({"error": "Not found"}), 404
+
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
     if jackson_family.delete_member(member_id):
         return jsonify({"done": True}), 200
     else:
-        return jsonify({"error": "Not found"}), 404
+        return jsonify({"error": "Member not found"}), 404
+
+
 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/datastructures.py
+++ src/datastructures.py
@@ -8,47 +8,65 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
-class FamilyStructure:
 
+class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-        self._members = [{
-            "id": self._generateId(),
-            "first_name": "John",
-            "last_name": last_name,
-            "age": 33,
-            "lucky_numbers": [7, 13, 22]
-        }, {
-            "id": self._generateId(),
-            "first_name": "Jane",
-            "last_name": last_name,
-            "age": 35,
-            "lucky_numbers": [10, 14, 3]
-        }, {
-            "id": self._generateId(),
-            "first_name": "Jimmy",
-            "last_name": last_name,
-            "age": 5,
-            "lucky_numbers": [1]
-        }]
+        # Lista de ejemplo de todos los miembros dados
+        self._members = [
+            {
+                "id": self._generateId(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
+   
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        member["id"] = self._generateId()
+        
+        if "id" not in member:
+            member["id"] = self._generateId()
         self._members.append(member)
 
     def delete_member(self, id):
-        self._members = [member for member in self._members if member["id"] != id]
+
+      
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                return True
+        return False
 
     def update_member(self, id, member):
-        for i, mem in enumerate(self._members):
-            if mem["id"] == id:
-                self._members[i] = member
-                break
+
+       
+        for index, m in enumerate(self._members):
+            if m["id"] == id:
+                self._members[index] = member
+                return True
+        return False
 
     def get_member(self, id):
+       
         for member in self._members:
             if member["id"] == id:
                 return member
